d03de2f32980ccbd32c01465f9cbe642ce3a980e
Nuno Candido <68172703+nfcandido@users.noreply.github.com>
2021-01-24 08:25:19 +0000

    New taskwarriror alias - wrkt
    wrkt - Create a new work task

--- .config/bash_it/aliases/taskwarrior.aliases.bash
+++ .config/bash_it/aliases/taskwarrior.aliases.bash
@@ -9,6 +9,8 @@ function _set_taskwarrior_aliases()
   if _command_exists task; then
     # Quick add to inbox
     alias tin='task add +in'
+    # Quick add a work task
+    alias wrkt='task add +wrk'
     # Add a ticker entry to inbox
     alias tick=tickle
     # Add a tickler entry for tomorrow (good when ypu want to think before making a decision)
